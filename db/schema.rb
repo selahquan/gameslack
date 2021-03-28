2223bff194e0ee3d7f324ad7ca5f00807f2dc23a
Josh <josh.handon@gmail.com>
2021-03-28 15:38:42 -0400

    removed password column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_182304) do
+ActiveRecord::Schema.define(version: 2021_03_28_193609) do
 
   create_table "games", force: :cascade do |t|
     t.string "slug"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2021_03_04_182304) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
-    t.string "password"
   end
 
 end
